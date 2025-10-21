96f2fb0ba3ff46b3ea05a70bbd452f31141f1686
mstanley-yo <mstanley.yo@gmail.com>
2025-10-21 10:59:08 +0900

    update ui to bslib

--- app.R
+++ app.R
@@ -1,147 +1,114 @@
 # Load R packages
 library(shiny)
-library(bslib) # for column_layout
-library(shinythemes)
+library(bslib)
 library(dplyr) # for mutate(), rename()
 library(tidyr) # for drop_na()
 library(ggplot2) # for ggplot()
 library(ggprism)
 library(flextable)
 
-# define github_link
-github_link <- "https://github.com/mstanley-yo/satolab-infectivity-helper"
+github_link <- tags$a(
+    shiny::icon("github"), "GitHub",
+    href = "https://github.com/mstanley-yo/satolab-infectivity-helper",
+    target = "_blank"
+)
 
-# define round volumes function
-round_volumes <- function(vol) {
-    case_when(
-        vol < 20 ~ round(vol, 2), # p20
-        vol > 200 ~ round(vol, 0), # p1000
-        .default = round(vol, 1) # p200
-    )
-}
+rm_samples_button <- actionButton(
+    "remove_all_samples",
+    "Remove All Samples",
+    icon = icon("trash")
+)
 
 # Define UI #####
-ui <- fluidPage(
-    theme = shinytheme("cerulean"),
-    navbarPage(
-        "Infectivity calculator",
-        
-        # tab panel to input standard curve information.
-        tabPanel(
-            "Standard curve",
-            
-            # Input RLU of serially diluted standard
-            sidebarPanel(
-                tags$h4("RLU of serially diluted standard:"),
-                numericInput(
-                    "std1", "1141.1 ng/mL (stock):", value = NA, min = 0
-                ), # test: 4.82E+06
-                numericInput(
-                    "std2", "570.6 ng/mL:", value = NA, min = 0
-                ), # test: 3.05E+06
-                numericInput(
-                    "std3", "285.3 ng/mL:", value = NA, min = 0
-                ), # test: 1.79E+06
-                numericInput(
-                    "std4", "142.6 ng/mL:", value = NA, min = 0
-                ), # test: 1.03E+06
-                numericInput(
-                    "std5", "71.3 ng/mL:", value = NA, min = 0
-                ), # test: 5.56E+05
-                numericInput(
-                    "std6", "0 ng/mL:", value = NA, min = 0
-                ) # test: 3.00E+01
+ui <- page_navbar(
+    title = "Infectivity calculator",
+    theme = bs_theme(bootswatch = "flatly"),
+    
+    # Standard curve tab ----
+    nav_panel(
+        "Standard curve",
+        layout_columns(
+            col_widths = c(4, 8),
+            card(
+                card_header("RLU of serially diluted standard:"),
+                numericInput("std1", "1141.1 ng/mL (stock):", NA, min = 0),
+                numericInput("std2", "570.6 ng/mL:", NA, min = 0),
+                numericInput("std3", "285.3 ng/mL:", NA, min = 0),
+                numericInput("std4", "142.6 ng/mL:", NA, min = 0),
+                numericInput("std5", "71.3 ng/mL:", NA, min = 0),
+                numericInput("std6", "0 ng/mL:", NA, min = 0)
             ),
-            
-            # Show standard curve
-            mainPanel(
-                h3("Standard curve"),
-                h5("Typical slope is 0.0002 ~ 0.0003"),
-                h5("Typical intercept is -100 ~ 0"),
-                plotOutput("rlu_plot"),
+            card(
+                card_header("Standard curve"),
+                plotOutput("rlu_plot")
             )
-        ), # Navbar 1, tabPanel standard curve
-        tabPanel(
-            "HiBiT normalization",
-            sidebarPanel(
-                # Manual input
-                h4("Manual input"),
-                layout_columns(
-                    textInput(
-                        "sample_id_input", "Sample ID", value = ""
-                    ), # test: GD/1/2019
-                    numericInput(
-                        "rlu_input", "RLU", value = NA, min = 0
-                    ), # test 1.37E+06
-                ),
-                actionButton("add_sample", "Add Sample", icon = icon("plus")),
-                br(), br(),
-                
-                # Allow input from excel
-                h4("Input from Excel"),
-                textInput(
-                    "excel_input", "Copy and paste from Excel", value = ""
-                ), # test: virus1	2.57E+04 virus2	2.92E+04
-                p("Ensure there are no white spaces in the virus names."),
-                p("For example: 'virus1	2.57E+04 virus2	2.92E+04'."),
-                actionButton(
-                    "add_sample_excel", 
-                    "Add Sample from Excel", 
-                    icon = icon("file-excel")
-                ),
-                br(), br(),
-                
-                # Dilution settings
-                h4("Dilution settings"),
-                numericInput(
-                    "pre_hibit_input", 
-                    "Dilution before HiBiT assay (times)", 
-                    value = 4, 
-                    min = 0
-                ),
-                layout_columns(
-                    numericInput(
-                        "target_volume_uL", "Target volume (uL)", value = 300
+        )
+    ),
+    
+    # HiBiT normalization tab ----
+    nav_panel(
+        "HiBiT normalization",
+        layout_columns(
+            col_widths = c(4, 8),
+            layout_columns(
+                col_widths = c(12, 12),
+                navset_card_pill(
+                    nav_panel(
+                        "Manual input",
+                        textInput("sample_id_input", "Sample ID", ""),
+                        numericInput("rlu_input", "RLU", NA, min = 0),
+                        actionButton(
+                            "add_sample", 
+                            "Add Sample", 
+                            icon = icon("plus")
+                        ),
+                        rm_samples_button
                     ),
-                    numericInput(
-                        "target_p24_ng_mL", "Target p24 (ng/mL)", value = 400
-                    ),
+                    nav_panel(
+                        "Input from Excel",
+                        textInput("excel_input", "Copy from Excel", ""),
+                        p("Ensure no spaces in virus names"),
+                        p("For example: 'V1\t2.57E+04 V2\t2.92E+04'..."),
+                        actionButton(
+                            "add_sample_excel",
+                            "Add Sample from Excel",
+                            icon = icon("file-excel")
+                        ),
+                        rm_samples_button
+                    )
                 ),
-                actionButton(
-                    "remove_all_samples",
-                    "Remove All Samples", 
-                    icon = icon("trash")
-                )
-            ), # sidebarPanel
-            
-            # main panel, show pretty output directly.
-            mainPanel(
-                #tableOutput("sample_table"),
-                uiOutput("pretty_output"),
-                br(),
-                downloadButton("download_docx", "Download table as .docx")
-            )
-        ), # Navbar, tabpanel HiBiT calculator
-        
-        # About page, show github
-        tabPanel(
-            "About",
-            mainPanel(
-                h3("About this website"),
-                p("Written in R Shiny by Maximilian Stanley Yo."),
-                p(
-                    "Follow development here: ",
-                    tags$a(
-                        "GitHub Repository", 
-                        href = github_link, 
-                        target = "_blank"
+                card(
+                    card_header("Dilution settings"),
+                    numericInput(
+                        "pre_hibit_input",
+                        "Dilution before HiBiT assay (times)",
+                        value = 4,
+                        min = 0
+                    ),
+                    layout_columns(
+                        numericInput(
+                            "target_volume_uL", 
+                            "Target volume (uL)", 
+                            value = 300
+                        ),
+                        numericInput(
+                            "target_p24_ng_mL", 
+                            "Target p24 (ng/mL)", 
+                            value = 400)
                     )
                 )
+            ),
+            card(
+                card_header("Dilution Table"),
+                uiOutput("table_output"),
+                br(),
+                downloadButton("download_docx", "Download table as .docx"),
+                p("Written in R Shiny by Maximilian Stanley Yo."),
+                github_link
             )
-        ) # Navbar, tabPanel About
-    ) # navbarPage
-) # fluidPage
-
+        )
+    )
+)
 
 # Define server #####
 server <- function(input, output) {
@@ -303,6 +270,15 @@ server <- function(input, output) {
             stringsAsFactors = FALSE
         ))
     })
+    
+    # define round volumes function
+    round_volumes <- function(vol) {
+        case_when(
+            vol < 20 ~ round(vol, 2), # p20
+            vol > 200 ~ round(vol, 0), # p1000
+            .default = round(vol, 1) # p200
+        )
+    }
 
     # Create flextable for output in both pretty output and download
     table_obj <- reactive({
@@ -354,13 +330,12 @@ server <- function(input, output) {
     })
 
     # Render table_obj() as HTML object for pretty output
-    output$pretty_output <- renderUI({
+    output$table_output <- renderUI({
         # validate that there is a standard curve.
         validate_stdcurve()
 
         # Render as HTML widget
         tagList(
-            h4("Dilution Table"),
             flextable::htmltools_value(table_obj())
         )
     })
